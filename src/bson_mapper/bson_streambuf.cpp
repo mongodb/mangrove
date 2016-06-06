5c99e537f7999163e2a6db4d1b3161db668a5133
Raphael Kargon <raphaelkargon@MacBook-Pro-34.local>
2016-06-06 11:53:48 -0400

    Adding BSON Output Streambuf

---
+++ src/bson_mapper/bson_streambuf.cpp
@@ -0,0 +1,80 @@
+// Copyright 2016 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <stdexcept>
+#include <streambuf>
+
+#include <bson.h>
+#include "bson_streambuf.hpp"
+
+#include <bson_mapper/config/prelude.hpp>
+
+namespace bson_mapper {
+BSON_MAPPER_INLINE_NAMESPACE_BEGIN
+
+bson_output_streambuf::bson_output_streambuf(document_callback cb)
+    : _cb(cb), _data(nullptr, [](uint8_t *p) {}), _bytes_read(0), _len(0) {
+}
+
+int bson_output_streambuf::underflow() {
+    return EOF;
+}
+
+int bson_output_streambuf::overflow(int ch) {
+    int result = EOF;
+    if (ch < 0 || ch > std::numeric_limits<uint8_t>::max()) {
+        throw std::invalid_argument("Character is out of bounds.");
+    }
+    assert(ch >= 0 && ch <= std::numeric_limits<uint8_t>::max());
+    result = insert(ch);
+    return result;
+}
+
+int bson_output_streambuf::insert(int ch) {
+    _bytes_read++;
+
+    // For the first four bytes, this builds int32 that contains the document size.
+    if (_bytes_read <= 4) {
+        _len |= (ch << (8 * (_bytes_read - 1)));
+    }
+    // Once the document size is received, allocate space.
+    if (_bytes_read == 4) {
+        if (_len > BSON_MAX_SIZE) {
+            throw std::invalid_argument("BSON document length is too large.");
+        }
+        _data = std::unique_ptr<uint8_t, void (*)(std::uint8_t *)>(new uint8_t[_len],
+                                                                   [](uint8_t *p) { delete[] p; });
+        std::memcpy(_data.get(), &_len, 4);
+    }
+
+    if (_bytes_read > 4) {
+        _data.get()[_bytes_read - 1] = static_cast<uint8_t>(ch);
+    }
+
+    // This creates the document from the given bytes, and calls the user-provided callback.
+    if (_bytes_read == _len) {
+        _cb({std::move(_data), _len});
+        _bytes_read = 0;
+        _len = 0;
+    } else if (_bytes_read > _len && _len >= 4) {
+        return EOF;
+    }
+    return ch;
+}
+
+BSON_MAPPER_INLINE_NAMESPACE_END
+}  // namespace bson_mapper
---
+++ src/bson_mapper/bson_streambuf.hpp
@@ -0,0 +1,79 @@
+// Copyright 2016 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/collection.hpp>
+
+#include <bson_mapper/config/prelude.hpp>
+
+namespace bson_mapper {
+BSON_MAPPER_INLINE_NAMESPACE_BEGIN
+
+/**
+* A streambuffer that accepts bytes of BSON data,
+* and passes a document into the user-provided callback when
+* all bytes are sent over.
+*/
+class BSON_MAPPER_API bson_output_streambuf : public std::streambuf {
+   public:
+    using document_callback = std::function<void(bsoncxx::document::value)>;
+
+    /**
+    * Constructs a new BSON Output Streambuffer
+    * that inserts documents into the given collection.
+    * @param coll A pointer to a MongoDB collection.
+    */
+    bson_output_streambuf(document_callback cb);
+
+    /**
+    * This function is called when writing to the stream and the buffer is full.
+    * Since we don't define a buffer, this is called with every character.
+    * @param  ch The byte of BSON to insert.
+    * @return    The inserted byte, or EOF if something failed.
+    */
+    int overflow(int ch) override;
+
+    /**
+    * This function always returns EOF,
+    * since one should not write from an output stream.
+    * @return EOF
+    */
+    virtual int underflow() override;
+
+   private:
+    /**
+    * This function inserts a byte of BSON data into the buffer.
+    * The first four bytes are stored in an int, and determine the document size.
+    * Then, that number of bytes (minus the first 4) are stored in the buffer.
+    * When the data is complete a BSON document value is created, and passed to the user-provided
+    * callback.
+    *
+    * @param  ch The byte to insert.
+    * @return    The byte inserted, or EOF if something failed.
+    */
+    BSON_MAPPER_PRIVATE int insert(int ch);
+    // This accepts a document::value and returns void.
+    document_callback _cb;
+    std::unique_ptr<uint8_t, void (*)(std::uint8_t*)> _data;
+    size_t _len = 0;
+    size_t _bytes_read = 0;
+};
+
+BSON_MAPPER_INLINE_NAMESPACE_END
+}  // namespace bson_mapper
+
+#include <bson_mapper/config/postlude.hpp>
