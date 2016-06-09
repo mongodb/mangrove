d3b0703e2c81de84be1de6093fa4f1695d954bf8
Raphael Kargon <raphael.kargon@10gen.com>
2016-06-09 16:54:59 -0400

    Removed dependency on mongocxx from unit test

--- src/bson_mapper/test/bson_streambuf.cpp
+++ src/bson_mapper/test/bson_streambuf.cpp
@@ -18,70 +18,46 @@
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/types.hpp>
 
-#include <mongocxx/client.hpp>
-#include <mongocxx/collection.hpp>
-#include <mongocxx/instance.hpp>
-#include <mongocxx/uri.hpp>
-
 using namespace bson_mapper;
 
 /**
- * A callable object that represents a callback that inserts a document into a collection.
- * The object is constructed with a given collection, and when called inserts document values into
- * that collection.
- *
- * This is used to test the bson_output_streambuf class.
+ * A callable object that compares a given BSON document to an original reference.
+ * This is used for unit testing.
  */
-class insert_callback {
+class doc_validator_callback {
    public:
-    insert_callback(mongocxx::collection c) : _c(c) {
+    doc_validator_callback(bsoncxx::document::value doc) : _doc(doc) {
     }
 
     /**
-     * Inserts the given document value into the collection.
+     * This asserts that the given document is the same as the reference document.
      */
     void operator()(bsoncxx::document::value v) {
-        _c.insert_one(v.view());
+        REQUIRE(v.view() == _doc.view());
     }
 
    private:
-    mongocxx::collection _c;
+    bsoncxx::document::value _doc;
 };
 
-TEST_CASE("bson_streambuf can insert a document into a collection",
+TEST_CASE("bson_streambuf can faithfully transfer a document",
           "[bson_mapper::bson_output_streambuf]") {
-    // init database
-    mongocxx::instance inst{};
-    mongocxx::client conn{mongocxx::uri{}};
-    auto collection = conn["testdb"]["testcollection"];
-    std::function<void(bsoncxx::document::value)> cb = insert_callback(collection);
-
     // set up test BSON document
     std::string json_str = "{\"a\": 1, \"b\":[1,2,3], \"c\": {\"a\": 1}}";
     auto bson_obj = bsoncxx::from_json(json_str);
-    auto orig_view = bson_obj.view();
-    const uint8_t *data = orig_view.data();
-    int len = orig_view.length();
+    auto bson_view = bson_obj.view();
+    const uint8_t *data = bson_view.data();
+    int len = bson_view.length();
+
+    // set up callback
+    doc_validator_callback cb(bson_obj);
 
     // set up stream
     bson_output_streambuf b_buff(cb);
     std::ostream doc_stream(&b_buff);
 
-    // print coll's document count before insert
-    auto cursor = collection.find({});
-    int original_count = std::distance(cursor.begin(), cursor.end());
-
     // write document to stream
     doc_stream.write(reinterpret_cast<const char *>(data), len);
-
-    cursor = collection.find({});
-    int first_insert_count = std::distance(cursor.begin(), cursor.end());
-    REQUIRE(first_insert_count == original_count + 1);
-
     // write document second time
     doc_stream.write(reinterpret_cast<const char *>(data), len);
-
-    cursor = collection.find({});
-    int second_insert_count = std::distance(cursor.begin(), cursor.end());
-    REQUIRE(second_insert_count == first_insert_count + 1);
 }
